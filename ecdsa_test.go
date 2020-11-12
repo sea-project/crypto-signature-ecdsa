50a1ed224f2dc243a9179e25f7591370c2030d8e
biwow <biwow@qq.com>
2020-11-12 21:46:14 +0800

    ecdsa s256

---
+++ ecdsa_test.go
@@ -0,0 +1,63 @@
+package ecdsa
+
+import (
+	"encoding/hex"
+	ecc "github.com/sea-project/crypto-ecc-s256"
+	"testing"
+)
+
+func TestGenerateKey(t *testing.T) {
+	prv, pub := GenerateKey()
+	t.Log("私钥原型：", prv.ToHex())
+	t.Log("公钥原型：", pub.ToHex())
+	t.Log("私钥转公钥：", prv.ToPubKey().ToHex())
+
+	pubHex := pub.ToHex()
+	t.Log("公钥哈希：", pubHex)
+
+	pubs, err := HexToPubKey(pubHex)
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log("公钥还原：", pubs.ToHex())
+
+	prvHex := prv.ToHex()
+	t.Log("私钥哈希：", prvHex)
+
+	prvs, err := HexToPrvKey(prvHex)
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log("还原私钥：", prvs.ToHex())
+}
+
+func TestPrivateKey_Sign(t *testing.T) {
+	prv, pub := GenerateKey()
+	t.Log("原公钥：", pub.ToHex())
+	hash := []byte("1111111111111")
+
+	// 第一种签名
+	signature, err := prv.Sign(hash)
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log("第一种签名：", signature.S, signature.R)
+
+	isCheck := signature.Verify(hash, pub)
+	t.Log(isCheck)
+
+	// 第二种签名方式
+	sign, err := SignCompact(ecc.S256(), prv, hash, true)
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log("第二种签名：", hex.EncodeToString(sign))
+	t.Log("第二种签名：", len(sign))
+
+	pubs, istrue, err := RecoverCompact(ecc.S256(), sign, hash)
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log("结果：", istrue)
+	t.Log("导公钥：", pubs.ToHex())
+}
---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/sea-project/crypto-signature-ecdsa
+
+go 1.13
+
+require (
+	github.com/sea-project/crypto-ecc-s256 v0.0.0-20201112053750-eac7c730f85f
+	github.com/sea-project/sea-pkg v0.0.0-20200929032655-f6a69397212e
+)
